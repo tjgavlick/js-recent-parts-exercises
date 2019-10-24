1360e8f4e5f7954c37885c7e26e4d01e2f040ff1
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2019-10-24 15:18:38 -0700

    Add my solution for iterators/generators exercise

--- iterators-generators/ex.js
+++ iterators-generators/ex.js
@@ -1,5 +1,9 @@
 var numbers = {
-	// ..
+	*[Symbol.iterator] ({ start = 0, end = 100, step = 1} = {}) {
+    for (let i = start; i <= end; i += step) {
+      yield i;
+    }
+  }
 };
 
 // should print 0..100 by 1s
@@ -8,7 +12,4 @@ for (let num of numbers) {
 }
 
 // should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
-
-// Hint:
-//     [...numbers[Symbol.iterator]( ?? )]
+console.log(`My lucky numbers are: ${[ ...numbers[Symbol.iterator]({ start: 6, end: 30, step: 4 }) ]}`);
